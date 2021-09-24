add2c8b4aeb456af38b3d7c0b3cfdcb7a2772b00
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-09-24 13:12:40 -0700

    use modal for form #2

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,5 +1,44 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure } from '@chakra-ui/react';
+  import { useState } from 'react';
+
 function Dashboard(props) {
-  return <h1>Dashboard</h1>
+  const { isOpen, onOpen, onClose } = useDisclosure({
+    onOpen: () => console.log('tesefsdfsdt')
+  });
+  // const test = () => {
+  //   onOpen();
+  //   console.log('test');
+  // };
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <Button onClick={onOpen}>Open Modal</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            asdasdasd
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Save
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 export default Dashboard;
