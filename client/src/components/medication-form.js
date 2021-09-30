7a225229e13318a2a906cb462aa33531e576813b
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-09-29 22:23:31 -0700

    used formik to handle changes #2

--- client/src/components/medication-form.js
+++ client/src/components/medication-form.js
@@ -1,26 +1,58 @@
-import {
-  FormControl,
-  FormLabel,
-  Input
-} from '@chakra-ui/react';
+// import {
+//   FormControl,
+//   FormLabel,
+//   Input
+// } from '@chakra-ui/react';
 
-function MedicationForm(props) {
-  return (
-    <>
-      <FormControl>
-        <FormLabel>Medication</FormLabel>
-        <Input placeholder="medication name" />
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Dose</FormLabel>
-        <Input placeholder="dose" />
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Frequency</FormLabel>
-        <Input placeholder="frequency" />
-      </FormControl>
-    </>
-  );
-}
+// function MedicationForm(props) {
+//   return (
+    // <>
+    //   <FormControl>
+    //     <FormLabel>Medication</FormLabel>
+    //     <Input placeholder="medication name" />
+    //   </FormControl>
+    //   <FormControl mt={4}>
+    //     <FormLabel>Dose</FormLabel>
+    //     <Input placeholder="dose" />
+    //   </FormControl>
+    //   <FormControl mt={4}>
+    //     <FormLabel>Frequency</FormLabel>
+    //     <Input placeholder="frequency" />
+    //   </FormControl>
+    // </>
+//   );
+// }
+// import React from 'react';
+// import { Formik } from 'formik';
 
-export default MedicationForm;
+// const MedicationForm = () => {
+//   const formik = useFormik({
+//     initialValues: {
+//       firstName: '',
+//       lastName: '',
+//       email: '',
+//     },
+//     onSubmit: values => {
+//       alert(JSON.stringify(values, null, 2));
+//     },
+//   });
+//   return (
+//     <Formik>
+//       <FormControl>
+//         <FormLabel>Medication</FormLabel>
+//         <Input placeholder="medication name" />
+//       </FormControl>
+//       <FormControl mt={4}>
+//         <FormLabel>Dose</FormLabel>
+//         <Input placeholder="dose" />
+//       </FormControl>
+//       <FormControl mt={4}>
+//         <FormLabel>Frequency</FormLabel>
+//         <Input placeholder="frequency" />
+//       </FormControl>
+//     </Formik>
+
+//   );
+// };
+
+// export default MedicationForm;
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -7,30 +7,31 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  CheckboxGroup,
+  HStack,
   useDisclosure } from '@chakra-ui/react';
   import { useState } from 'react';
+  import { useFormik } from 'formik';
   import MedicationForm from '../components/medication-form';
 
 function Dashboard() {
-  // const getForm = (action) => {
-  //   if (action === 'add') {
-  //     return (
-  //       <FormControl>
-  //         <FormLabel>First name</FormLabel>
-  //         <Input placeholder="First name" />
-  //       </FormControl>
-  //     );
-  //   } else {
-  //     return (
-  //       <FormControl>
-  //         <FormLabel>Last name</FormLabel>
-  //         <Input placeholder="last name" />
-  //       </FormControl>
-  //     );
-  //   }
-  // };
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [action, setAction] = useState();
+  const formik = useFormik({
+    initialValues: {
+      medication: '',
+      dose: '',
+      frequency: []
+    },
+    onSubmit: values => {
+      // alert(JSON.stringify(values, null, 2));
+      console.log('formik vbalues: ' + JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
     <>
@@ -39,23 +40,104 @@ function Dashboard() {
         const action = e.target.getAttribute("id");
         setAction(action);
         onOpen();
-      }}>Open Modal</Button>
+      }}>Add</Button>
       <Button id="edit" onClick={(e) => {
         const action = e.target.getAttribute("id");
         setAction(action);
         onOpen();
-      }}>edit</Button>
+      }}>Edit</Button>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{`${action} Medicine`}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <MedicationForm action={action} />
+            <FormControl>
+              <FormLabel>Medication</FormLabel>
+              <Input
+                id="medication"
+                placeholder="medication name"
+                onChange={formik.handleChange}
+                value={action === 'add' ? formik.values.medication : 'edit'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Dose</FormLabel>
+              <Input
+                id="dose"
+                placeholder="dose"
+                onChange={formik.handleChange}
+                value={action === 'add' ? formik.values.dose : 'edit'}
+              />
+            </FormControl>
+            <CheckboxGroup colorScheme="blue">
+              <HStack mt={4}>
+                <Checkbox
+                  id="monday"
+                  value="monday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Monday
+                </Checkbox>
+                <Checkbox
+                  id="tuesday"
+                  value="tuesday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Tuesday
+                </Checkbox>
+                <Checkbox
+                  id="wednesday"
+                  value="wednesday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Wednesday
+                </Checkbox>
+                <Checkbox
+                  id="thursday"
+                  value="thursday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Thursday
+                </Checkbox>
+              </HStack>
+            </CheckboxGroup>
+                <CheckboxGroup colorScheme="blue">
+                  <HStack mt={4}>
+                <Checkbox
+                  id="friday"
+                  value="friday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Friday
+                </Checkbox>
+                <Checkbox
+                  id="saturday"
+                  value="saturday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Saturday
+                </Checkbox>
+                <Checkbox
+                  id="sunday"
+                  value="sunday"
+                  name="frequency"
+                  onChange={formik.handleChange}
+                >
+                  Sunday
+                </Checkbox>
+                  </HStack>
+                </CheckboxGroup>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={() => {
-              console.log('submit form');
+            <Button type="submit" colorScheme="blue" mr={3} onClick={() => {
+              formik.handleSubmit();
               onClose();
             }}>
               Save
