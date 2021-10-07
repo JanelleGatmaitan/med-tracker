e606d8dd3e96328e44e3a6e48e41e1a7c66c575f
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-10-06 22:52:10 -0700

    use yup for validationschema #2

--- client/src/components/medication-form.js
+++ client/src/components/medication-form.js
@@ -1,58 +1,127 @@
-// import {
-//   FormControl,
-//   FormLabel,
-//   Input
-// } from '@chakra-ui/react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    Input,
+    Button,
+    Checkbox,
+    CheckboxGroup,
+    HStack
+} from "@chakra-ui/react"
 
-// function MedicationForm(props) {
-//   return (
-    // <>
-    //   <FormControl>
-    //     <FormLabel>Medication</FormLabel>
-    //     <Input placeholder="medication name" />
-    //   </FormControl>
-    //   <FormControl mt={4}>
-    //     <FormLabel>Dose</FormLabel>
-    //     <Input placeholder="dose" />
-    //   </FormControl>
-    //   <FormControl mt={4}>
-    //     <FormLabel>Frequency</FormLabel>
-    //     <Input placeholder="frequency" />
-    //   </FormControl>
-    // </>
-//   );
-// }
-// import React from 'react';
-// import { Formik } from 'formik';
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
 
-// const MedicationForm = () => {
-//   const formik = useFormik({
-//     initialValues: {
-//       firstName: '',
-//       lastName: '',
-//       email: '',
-//     },
-//     onSubmit: values => {
-//       alert(JSON.stringify(values, null, 2));
-//     },
-//   });
-//   return (
-//     <Formik>
-//       <FormControl>
-//         <FormLabel>Medication</FormLabel>
-//         <Input placeholder="medication name" />
-//       </FormControl>
-//       <FormControl mt={4}>
-//         <FormLabel>Dose</FormLabel>
-//         <Input placeholder="dose" />
-//       </FormControl>
-//       <FormControl mt={4}>
-//         <FormLabel>Frequency</FormLabel>
-//         <Input placeholder="frequency" />
-//       </FormControl>
-//     </Formik>
+    const validationSchema = Yup.object({
+        name: Yup.string().required('Required'),
+        dose: Yup.string().required('Required'),
+        frequency: Yup.boolean().oneOf([true], 'Select at least one day')
+    });
 
-//   );
-// };
+    // function validateName(value) {
+    //     let error
+    //     if (!value) {
+    //         error = "Name is required"
+    //     } else if (value.toLowerCase() !== "naruto") {
+    //         error = "Jeez! You're not a fan 😱"
+    //     }
+    //     return error
+    // }
 
-// export default MedicationForm;
+    function getInitialValues(action) {
+        if (action === 'add') {
+            return {
+                name: '',
+                dose: '',
+                frequency: {
+                    monday: false,
+                    tuesday: false,
+                    wednesday: false,
+                    thursday: false,
+                    friday: false,
+                    saturday: false,
+                    sunday: false
+                }
+            }
+        }
+        return {
+            name: ''
+        }
+    }
+
+
+function MedicationForm({ closeModal, action }) {
+    return (
+        <Formik
+            initialValues={getInitialValues(action)}
+            validationSchema={validationSchema}
+            onSubmit={(values, actions) => {
+                setTimeout(() => {
+                    closeModal();
+                    console.log(JSON.stringify(values, null, 2))
+                    console.log('submit');
+                    actions.setSubmitting(false)
+                }, 1000)
+            }}
+        >
+            {(props) => (
+                <Form>
+                    <Field name="name">
+                        {({ field, form }) => (
+                            <FormControl isInvalid={form.errors.name && form.touched.name}>
+                                <FormLabel htmlFor="name">First name</FormLabel>
+                                <Input {...field} id="name" placeholder="name" />
+                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                            </FormControl>
+                        )}
+                    </Field>
+                    <Field name="dose">
+                        {({ field, form }) => (
+                            <FormControl isInvalid={form.errors.dose && form.touched.dose}>
+                                <FormLabel htmlFor="dose" mt={4}>Dose</FormLabel>
+                                <Input {...field} id="dose" placeholder="dose"/>
+                                <FormErrorMessage>{form.errors.dose}</FormErrorMessage>
+                            </FormControl>
+                        )}
+                    </Field>
+                    <Field name="frequency">
+                        {({ field, form }) => (
+                            <FormControl mt={4}  isInvalid={form.errors.frequency && form.touched.frequency}>
+                                <FormLabel>
+                                    <CheckboxGroup>
+                                        {days.map((day) => <CheckBox {...field} key={day} id={day} day={day} />)}
+                                    </CheckboxGroup>
+                                </FormLabel>
+                                <FormErrorMessage>{form.errors.frequency}</FormErrorMessage>
+                            </FormControl>
+                        )}
+                    </Field>
+                    <Button
+                        mt={4}
+                        colorScheme="blue"
+                        isLoading={props.isSubmitting}
+                        type="submit"
+                    >
+                        Save
+                    </Button>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+function CheckBox({ day, onChange }) {
+    return (
+        <Checkbox
+            id={day}
+            value={day}
+            name="frequency"
+            onChange={onChange}
+        >
+            {day}
+        </Checkbox>
+    );
+}
+
+export default MedicationForm;
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -4,35 +4,15 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Input,
-  Checkbox,
-  CheckboxGroup,
-  HStack,
   useDisclosure } from '@chakra-ui/react';
   import { useState } from 'react';
-  import { useFormik } from 'formik';
   import MedicationForm from '../components/medication-form';
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 function Dashboard() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [action, setAction] = useState();
-  const formik = useFormik({
-    initialValues: {
-      medication: '',
-      dose: '',
-      frequency: []
-    },
-    onSubmit: values => {
-      // alert(JSON.stringify(values, null, 2));
-      console.log('formik values: ' + JSON.stringify(values, null, 2));
-    },
-  });
 
   return (
     <>
@@ -53,54 +33,11 @@ function Dashboard() {
           <ModalHeader>{`${action} Medicine`}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <FormControl>
-              <FormLabel>Medication</FormLabel>
-              <Input
-                id="medication"
-                placeholder="medication name"
-                onChange={formik.handleChange}
-                value={action === 'add' ? formik.values.medication : 'edit'}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Dose</FormLabel>
-              <Input
-                id="dose"
-                placeholder="dose"
-                onChange={formik.handleChange}
-                value={action === 'add' ? formik.values.dose : 'edit'}
-              />
-            </FormControl>
-            <CheckboxGroup colorScheme="blue">
-              <HStack mt={4}>
-                {days.map((day) => <CheckBox key={day} day={day} onChange={formik.handleChange}/>)}
-               </HStack>
-            </CheckboxGroup>
+            <MedicationForm closeModal={onClose} action={action}/>
           </ModalBody>
-          <ModalFooter>
-            <Button type="submit" colorScheme="blue" mr={3} onClick={() => {
-              formik.handleSubmit();
-              onClose();
-            }}>
-              Save
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  );
-}
-
-function CheckBox({day, onChange}) {
-  return (
-    <Checkbox
-      id={day}
-      value={day}
-      name="frequency"
-      onChange={onChange}
-    >
-      {day}
-    </Checkbox>
   );
 }
 
